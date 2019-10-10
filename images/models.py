e20a0670ba2c924163ea4729c6208d84c6a4a12a
juru-juliette <kangabejuliette@gmail.com>
2019-10-10 10:41:57 +0200

    creating table of images

--- images/models.py
+++ images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'pictures/')
+    name = models.CharField(max_length =60)
+    description = models.TextField()
+    def save_image(self):
+        self.save()
+
