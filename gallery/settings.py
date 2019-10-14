03f15ed6e613b396644421dfb1f16fdc9773b5c8
juru-juliette <kangabejuliette@gmail.com>
2019-10-14 09:25:28 +0200

    checking my password

--- gallery/settings.py
+++ gallery/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gallery',
         'USER': 'wecode',
-    'PASSWORD':'  ',
+    'PASSWORD':'juruyuly1',
     }
 }
 
